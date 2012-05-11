c2551ca56d721784cafce046f867b02ac90a341e
LionelC <lionelc@admins-Mac-mini.local>
2012-05-11 17:18:08 -0400

    project commit

---
+++ Readme.txt
@@ -0,0 +1,18 @@
+//
+//  app_updateViewController.h
+//  app-update
+//
+//  Created by Marin Todorov on 3/27/10.
+//  Copyright Marin Todorov 2010. All rights reserved.
+//
+
+License:
+http://creativecommons.org/licenses/by/3.0/us/
+
+Attribution is not required.
+
+Author: Marin Todorov aco
+http://www.underplot.com
+
+Images: Nilay Petkov
+http://www.kunstdrom.com
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  app-update
+//
+//  Created by Marin Todorov on 3/27/10.
+//  Copyright Marin Todorov 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
